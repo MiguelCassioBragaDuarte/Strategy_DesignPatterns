10191305260612fdef020a6857d04b3e542a75f9
Miguel Duarte <miguelcassiob@gmail.com>
2026-03-21 15:50:48 -0300

    Criação do metodo de pagamento Dinheiro  do projeto WPF Sistema de Pagamentos Strategy

---
+++ Strategies/Dinheiro.cs
@@ -0,0 +1,9 @@
+namespace SistemaDePagamentos_Strategy.Strategies;
+
+public class Dinheiro: IPagamentoStrategy
+{
+    public string Pagar(double valor)
+    {
+        return $"Pagamento de R$ {valor} realizado no Dinhero.";
+    }
+}
