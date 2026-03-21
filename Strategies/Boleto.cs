fcf7a2e44bd7c13bd16c0230b7e05b5b426672ec
Miguel Duarte <miguelcassiob@gmail.com>
2026-03-21 15:33:39 -0300

    Criação do metoto de pagamento boleto do projeto WPF Sistema de Pagamentos Strategy

---
+++ Strategies/Boleto.cs
@@ -0,0 +1,9 @@
+namespace SistemaDePagamentos_Strategy.Strategies;
+
+public class Boleto: IPagamentoStrategy
+{
+    public string Pagar(double valor)
+    {
+        return $"Boleto gerado no valor de R$ {valor}.";
+    }
+}
